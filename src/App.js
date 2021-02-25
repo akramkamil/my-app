a239364edb5a28d6a1731a0ef096e8ded07b638d
akramkamil <akrambean@gmail.com>
2021-02-25 13:31:09 +0200

    fisrt push for my-app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = handleId => {
+    const counters = this.state.counters.filter(e => e.id !== handleId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounter={this.state.counters.filter(e => e.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+const NavBar = ({ totalCounter }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        NavBar{" "}
+        <span className="badge badge-pill badge-secondary">{totalCounter}</span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
